913ae77e18ad89cf31173be8b25fda1fd4c09900
BlenAndy <andnetblen@gmail.com>
2026-04-07 14:56:39 +0200

    changed - to / for testing

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def add(a, b):
 
 def subtract(a, b):
     """Return the difference of a and b."""
-    return a - b
+    return a / b
 
 def multiply(a, b):
     """Return the product of a and b."""
